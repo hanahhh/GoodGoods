dbce117f94c4c75ad3c29e163062543d0ae9c4dc
hanahhh <hoangnhung25122001@gmail.com>
2024-08-05 14:28:13 +0700

    mvn spotless:apply to deal with the sensitive convention of Github action trigger

--- catalog-service/src/test/java/com/hanah/catalog_service/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/hanah/catalog_service/domain/ProductRepositoryTest.java
@@ -3,7 +3,6 @@ package com.hanah.catalog_service.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,11 +19,11 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
-//    @Test
-//    void shouldGetAllProducts() {
-//        List<ProductEntity> products = productRepository.findAll();
-//        assertThat(products).hasSize(15);
-//    }
+    //    @Test
+    //    void shouldGetAllProducts() {
+    //        List<ProductEntity> products = productRepository.findAll();
+    //        assertThat(products).hasSize(15);
+    //    }
 
     @Test
     void shouldGetProductByCode() {
